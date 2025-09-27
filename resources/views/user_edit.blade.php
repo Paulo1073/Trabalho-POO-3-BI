3ab997347fddd1a819772875abc2595df2a57ab3
Paulo1073 <168665678+Paulo1073@users.noreply.github.com>
2025-09-27 14:11:07 -0300

    Aplicando a estilização

--- resources/views/user_edit.blade.php
+++ resources/views/user_edit.blade.php
@@ -3,22 +3,34 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/form.css') }}">
+    <title>Editar Usuário</title>
 </head>
 <body>
-    <h1>Editar<h1>
-    
-    @if(session()->has('message'))
-        {{session()->get('message')}}
-    @endif
-    <form action="{{route('users.update',['user'=> $user->id ])}}"  method="post">
-        @csrf
-        <input name="_method" value='PUT' type="hidden">
-        <input name="name" type="text"  value="{{$user->name}}">
-        <input name="email" type="text"  value="{{$user->email}}">
-        <input name="passeord" type="text"  value="{{$user->password}}">
-        <input type="submit" value="ATUALIZAR">
-
-    </form>
+    <div class="container">
+        <h2>Editar Usuário</h2>
+        
+        @if(session()->has('message'))
+            <div class="message">
+                {{session()->get('message')}}
+            </div>
+        @endif
+        <form action="{{route('users.update',['user'=> $user->id ])}}"  method="post">
+            @csrf
+            @method('PUT')
+            <div class="form-group">
+                <label for="name">Nome:</label>
+                <input name="name" id="name" type="text"  value="{{$user->name}}">
+            </div>
+            <div class="form-group">
+                <label for="email">Email:</label>
+                <input name="email" id="email" type="email"  value="{{$user->email}}">
+            </div>
+            <button type="submit">ATUALIZAR</button>
+        </form>
+        <a href="{{route('users.index')}}" class="btn-return">Retornar</a>
+    </div>
 </body>
 </html>
