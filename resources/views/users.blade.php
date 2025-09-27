b207bfb05c361afc6f16604ce1c73425e791a6b3
Paulo1073 <168665678+Paulo1073@users.noreply.github.com>
2025-09-27 14:11:49 -0300

    Aplicando a estilização

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -3,19 +3,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    <title>Usuários</title>
 </head>
 <body>
-    <h2>Usuarios</h2>
-    @if(session()->has('message'))
-        {{session()->get('message')}}
-    @endif
-    <ul>
-        @foreach ($users as $user)
-            <li ' >
-                Nome: {{ $user->name }} | Email:{{ $user->email }} | Senha:{{ $user->password }} | <a href="{{ route('users.edit', ['user' => $user->id]) }}">Edit</a> |  <a href="">Delete</a>
-            </li>
-        @endforeach
-    </ul>
+    <div class="container">
+        <h2>Usuários</h2>
+        <a href="{{route('users.create')}}" class="btn-create">Cadastrar</a>
+
+        @if(session()->has('message'))
+            <div class="message">
+                {{ session()->get('message') }}
+            </div>
+        @endif
+
+        <ul>
+            @foreach ($users as $user)
+                <li>
+                    <span>{{ $user->name }}</span>
+                    <div>
+                        <a href="{{ route('users.edit', ['user' => $user->id]) }}">Editar</a>
+                        <a href="{{route('users.show', ['user' => $user->id])}}">Mostrar</a>
+                    </div>
+                </li>
+            @endforeach
+        </ul>
+    </div>
 </body>
 </html>
